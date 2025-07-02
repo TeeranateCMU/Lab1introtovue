463db6ce824e9639781e821585af1b274c1c7a62
teeranate <teeranate_tho@cmu.ac.th>
2025-07-02 19:16:34 +0700

    2.5-3.6 add des, link, img  bind data using v-bind

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const {createApp, ref} = Vue;
 createApp ({
     setup() {
         const product = ref('boot')
+        const image = ref('./assets/images/socks_green.jpg')
         const description = ref('this is boots use to walk')
+        const click = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
+    
         return{
             product,
             inStock,
-            description
+            description,
+            image,
+            click
         }
     }
 
