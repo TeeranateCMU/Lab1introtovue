44f2f3787168a13314108809966743e2a1982b8a
teeranate <teeranate_tho@cmu.ac.th>
2025-07-03 01:05:46 +0700

    10.2 update setup in main.js

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ const app = createApp ({
     setup() {
         const cart = ref(0)
         const premium = ref(true)
+        function updateCart() {
+            cart.value +=1
+        }
         const details = ref(["50% cotton", "30% wool", "20% polyester"])
         return{
             cart,
             premium,
-            details
+            details,
+            updateCart
         }
     }
 })
